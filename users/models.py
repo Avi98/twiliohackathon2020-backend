28058c97bd6e1b8283566744018a7d32af28c63d
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-19 02:47:43 +0530

    mobile popup desing changes added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# user profile
+# first name, last name, 
+# description
+# email
+# contact info
+# current location
+class Profile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        image = models.ImageField(default='defaultProfileImg.png', upload_to='_profile_img', blank=True, null=True)
+        first_name = models.CharField(max_length=60, blank=True)
+        last_name = models.CharField(max_length=60, blank=True)
+        description = models.CharField(max_length=60, blank=True)
+        mobile = models.CharField(max_length=60, blank=True)
+        current_location = models.CharField(max_length=60, blank=True)
+        
--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from .models import Profile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,3 +27,21 @@ class LoginSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
         raise serializers.ValidationError('Incorrect Credentails')
+
+#profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(source='user.id')
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+    profile=Profile
+    # depth=2
+    class Meta:
+        model = Profile
+        fields= ('user','image', 'first_name', 'last_name', 'description','mobile', 'current_location','user_id')
+        read_only_fields=('user','user_id')
+
+        def create(self,validated_data):
+            user_id = validated_data.pop('user_id')
+            user = User.objects.create(user_id=user_id)
+            profile = Profile.objects.create(**validated_data)
+            return profile
