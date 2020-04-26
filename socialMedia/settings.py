1202e48ebdd2bfcbc99784d82adbe2e99297854a
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-26 21:38:38 +0530

    test setting added

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -146,6 +146,6 @@ STATICFILES_DIRS = (
 # cors enable
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST= ['http://localhost:3000', 'whispering-hollows-69916.herokuapp.com']
+CORS_ORIGIN_WHITELIST= ['http://localhost:3000', 'whispering-hollows-69916.herokuapp.com', 'https://hidden-reef-09190.herokuapp.com']
 
 django_heroku.settings(locals())
