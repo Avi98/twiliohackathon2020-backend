0afe7bf10a64730a670aa45d4b9178103c307c54
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-26 12:16:19 +0530

    ready for deployment

--- services/api.py
+++ services/api.py
@@ -7,23 +7,40 @@ from .serializers import ProductsSerializer
 from rest_framework.response import Response
 
 # Create your views here.
+
+
 class ServiceViewSet(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classess = [permissions.IsAuthenticated]
 
+    # all products
     def get(self, request):
-        products = Products()
-        productList = []
-        for products in Products.objects.all():
-            productList.append({
-                "name":products.name,
-                "sup_price": products.sup_price,
-                "selling_price": products.selling_price,
-                "manufacture": products.manufacture,
-                "description": products.description,
-                "onSale": products.onSale,
-                "user_id": products.user_id,
-                "user_name":products.name
-                })
-        return Response(productList)
+        serilaizer = ProductsSerializer(Products.objects.all(), many=True)
+        return Response(serilaizer.data)
 
+
+# CRUD Products
+class ProductsViewSet(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classess = [permissions.IsAuthenticated]
+
+    def get(self, response, pk):
+        if response.query_params.get('id'):
+            product = Products.objects.filter(user_id=pk, id = response.query_params.__getitem__('id'))
+            serilaizer = ProductsSerializer(product, many=True)
+            return Response(serilaizer.data)
+            
+        product = Products.objects.filter(user_id=pk)
+        serilaizer = ProductsSerializer(product, many=True)
+
+        return Response(serilaizer.data)
+
+    def post(self, request, pk):
+        serializer = ProductsSerializer(
+            context={'request': request}, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+    
--- services/urls.py
+++ services/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .api import ServiceViewSet
+from .api import ServiceViewSet, ProductsViewSet
 from knox import views as knox_views
 
 urlpatterns = [
     path('auth', include('knox.urls')),
-    path('productsList', ServiceViewSet.as_view()),
+    path('products', ServiceViewSet.as_view()),
+    path('product/<int:pk>', ProductsViewSet.as_view()),
    
 ]   
