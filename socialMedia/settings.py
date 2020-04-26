9889602651eeb2cf353ba13682b86197e6401b7e
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-26 20:24:21 +0530

    test setting added

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,5 @@ STATICFILES_DIRS = (
 # cors enable
 
 CORS_ORIGIN_WHITELIST= ['http://localhost:3000',]
+
+django_heroku.settings(locals())
